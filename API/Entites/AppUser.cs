5537f599cfe90a6b599a9703abe54c1925a4017c
York <yorkhsulin@gmail.com>
2022-04-30 16:47:04 +0800

    add register API

--- API/Entites/AppUser.cs
+++ API/Entites/AppUser.cs
@@ -9,5 +9,7 @@ namespace API.Entites
     {
         public int Id { get; set; }
         public string UserName { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
     }
 }
--- API/Startup.cs
+++ API/Startup.cs
@@ -52,7 +52,7 @@ namespace API
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIv5 v1"));
+                // app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIv5 v1"));
             }
 
             app.UseHttpsRedirection();
