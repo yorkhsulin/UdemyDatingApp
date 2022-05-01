c10bd919b57f20d274f91c2e85c2e0bf94106074
York <yorkhsulin@gmail.com>
2022-05-01 10:00:09 +0800

    creage login method

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -39,6 +39,26 @@ namespace API.Controllers
             await _context.SaveChangesAsync();
             return user;
         }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+        {
+            var user = await _context.Users
+            .SingleOrDefaultAsync(x=>x.UserName==loginDto.Username.ToLower());
+
+            if (user == null) return Unauthorized("Invaild Username") ; 
+
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+            var computeHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+            for (int i = 0 ; i<computeHash.Length;i++)
+            {
+                if (computeHash[i]!=user.PasswordHash[i]) return Unauthorized("Invaild Password");
+            }  
+
+            return user;
+
+        }
+
         private async Task<bool> UserExists(string username)
         {
             return await _context.Users.AnyAsync(x=>x.UserName == username.ToLower());
